027baa6387693bb5c307d54937efcb24218b4108
amanesis <amanesis@protonmail.com>
2023-05-25 15:15:42 +0300

    pyserial: working but only on PyCharm
    Sublime+CMD => ModuleNotFoundError: No module named 'serial'
    
    PyCharm => Working

--- main.py
+++ main.py
@@ -22,12 +22,12 @@ import pelco_p
 if __name__ == '__main__':
 
     #init gimbal try, problem with import serial  (import serial ModuleNotFoundError: No module named 'serial')
-    '''
-    ser = pyserial.Serial('COM4', 9600, timeout=1)  # Replace 'COM1' with the appropriate port and 9600 with the correct baud rate
-    frame = (0xA0, 0x00, 0x00, 0x00, 0x00, 0x00, 0xAF, 0x0F)
+
+    ser = serial.Serial('COM4', 9600, timeout=1)  # Replace 'COM1' with the appropriate port and 9600 with the correct baud rate
+    frame = (0xA0, 0x00, 0x00, 0x10, 0x00, 0x20, 0xAF, 0x3F)
     ser.write(frame)  # Send the command frame over the serial connection
     time.sleep(0.1)  # Wait for the command to be sent
-    '''
+
 
 
     
